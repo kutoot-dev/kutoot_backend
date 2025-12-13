7e042c94d42db580c788b90338eec209b993ac34
kutoot-dev <it@kutoot.com>
2025-12-13 17:23:37 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -60,7 +60,7 @@ class LoginController extends Controller
 
     public function logintrigger(Request $request)
     {
-        echo "testingprsdfkjalsdfkasdf";
+        echo "testingprsdfkjalsdfkasdf";die;
         $rules = [
             'identifier' => 'required',
         ];
