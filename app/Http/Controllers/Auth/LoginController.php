3f22870355649ada5b370f63447d4b9ea35d7b88
kutoot-dev <it@kutoot.com>
2025-12-13 20:52:36 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -187,7 +187,7 @@ if ($login_by == 'phone') {
 
     public function verifyOtp(Request $request)
     {
-        echo "testing google);
+        echo "testing google";
         print_r($request);die;
         // dd($request->all());
         // $rules = [
